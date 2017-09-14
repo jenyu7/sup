73919d1807931a6d6bbf8ff47afd862665c67538
Jen Yu <jyu7@stuy.edu>
2017-09-13 20:44:27 -0400

    added comments and xtra fxn

--- random_student.py
+++ random_student.py
@@ -1,3 +1,11 @@
+'''
+- How does water greet people? 
+- sup
+Shannon Lau and Jennifer Yu 
+SoftDev Period 7 
+HW #1: NO-body expects the Spanish Inquisition!
+2017-09-13
+'''
 import random
 
 CLASSES = {
@@ -6,9 +14,24 @@ CLASSES = {
         9: [ 'Yu Qi', 'Michela', 'Kristin', 'Fabiha', 'Maxim', 'Marcus', 'Ish', 'James', 'Ryan', 'Edward', 'Adeeb', 'Jake', 'Cynthia', 'Kevin', 'Levi', 'Edmond', 'Kyle', 'Andrew', 'Max', 'Jenny', 'Philip', 'Shan', 'Mansour', 'Ray', 'Jake', 'Ida', 'Kerry', 'Stanley', 'Jackie', 'William', 'Tina', 'Michael']
 }
 
+#picks a random student from a specified period 
 def random_student(period):
     return random.choice(CLASSES[period])
 
+'''
 print "7th period: " + random_student(7)
 print "8th period: " + random_student(8)
 print "9th period: " + random_student(9)
+'''
+
+#Functional for purposes of taking a random student from each class. 
+def random_per_period(): 
+	text = ""
+	text +=  "7th period: " + random_student(7) + "\n"
+	text +=  "8th period: " + random_student(8) + "\n"
+	text +=  "9th period: " + random_student(9) + "\n"
+	return text
+
+print random_per_period()
+
+#To create a program tasked with choosing a student for LCT, which seems to be one of the best applications of this random student generator, the program would have to retain some memory of the students who have already been chosen. 
